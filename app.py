b93cc17165da5c744686985af469a811c9ff648d
animalmanagementbouclelora2526 <animalmanagement.bouclelora@gmail.com>
2026-06-08 18:41:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -393,11 +393,6 @@ def breeder_animals(breeder_id):
             print("MATCH FOUND")
 
             animal['Username'] = breeder.get('Username', '') if breeder else ''
-            history = animal.get('PositionHistory', '')
-            if history and history != 'EMPTY':
-                animal['PositionList'] = history.split(';')
-            else:
-                animal['PositionList'] = []
 
             animals.append(animal)
 
